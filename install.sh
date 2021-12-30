62d798c38d4f6df2dd58d5f74bad06775833fe1a
Dhanesh Sivasamy <dhaneshsivasamy07@gmail.com>
2021-12-29 23:39:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/
-git clone https://github.com/cyberwr3nch/tmux_tweaks
+git clone https://github.com/dhaneshsivasamy07/tmux_tweaks
 cd /opt/
 git clone https://github.com/tmux-plugins/tmux-yank
 cp ~/tmux_tweaks/tmux.conf ~/.tmux.conf
