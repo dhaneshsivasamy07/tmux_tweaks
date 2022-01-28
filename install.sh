e8fdf68dfa673b47415a7fed27480409bf450c35
dhaneshsivasamy07 <dhaneshsivasamy07@gmail.com>
2022-01-28 06:09:24 -0500

    :dart: Modified prompts with reference to KaliLinux

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd ~/
 git clone https://github.com/dhaneshsivasamy07/tmux_tweaks
 cd /opt/
-git clone https://github.com/tmux-plugins/tmux-yank
+git clone https://github.com/tmux-plugins/tmux-yank 2>/dev/null
 cp ~/tmux_tweaks/tmux.conf ~/.tmux.conf
 mkdir ~/.tmux
 cp -r ~/tmux_tweaks/files/* ~/.tmux
