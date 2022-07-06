d88875d436abc12ba1c8243762866fe6801c3bf5
dhaneshsivasamy07 <dhaneshsivasamy07@gmail.com>
2022-07-06 14:05:54 +0530

    Chmod

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ git clone https://github.com/tmux-plugins/tmux-yank 2>/dev/null
 cp ~/tmux_tweaks/tmux.conf ~/.tmux.conf
 mkdir ~/.tmux
 cp -r ~/tmux_tweaks/files/* ~/.tmux
+chmod +x ~/.tmux/*
 cp ~/tmux_tweaks/opt_README.md /opt/README.md
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Done :)"
