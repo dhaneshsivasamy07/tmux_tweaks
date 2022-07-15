52a6b69a7ffb37033e15ddb5c38c803f9ebb3e8b
Dhanesh Sivasamy <dhaneshsivasamy07@gmail.com>
2022-07-15 01:05:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@ git clone https://github.com/dhaneshsivasamy07/tmux_tweaks
 sudo chown $(whoami) /opt
 cd /opt/
 git clone https://github.com/tmux-plugins/tmux-yank 2>/dev/null
-cp ~/tmux_tweaks/tmux.conf ~/.tmux.conf
+cp ~/tmux_tweaks/mytmux.conf ~/.tmux.conf
 mkdir ~/.tmux
 cp -r ~/tmux_tweaks/files/* ~/.tmux
 chmod +x ~/.tmux/*
 cp ~/tmux_tweaks/opt_README.md /opt/README.md
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+rm -rf ~/tmux_tweaks
 echo "Done :)"
 
