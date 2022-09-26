edd147572dc6234c4b548d955d3c93037240568b
Dhanesh Sivasamy <dhaneshsivasamy07@gmail.com>
2022-09-26 10:59:23 +0530

    Removed TMUX-YANK clone

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 
 cd ~/
 git clone https://github.com/dhaneshsivasamy07/tmux_tweaks
-sudo chown $(whoami) /opt
-cd /opt/
-git clone https://github.com/tmux-plugins/tmux-yank 2>/dev/null
 cp ~/tmux_tweaks/mytmux.conf ~/.tmux.conf
 mkdir ~/.tmux
 cp -r ~/tmux_tweaks/files/* ~/.tmux
